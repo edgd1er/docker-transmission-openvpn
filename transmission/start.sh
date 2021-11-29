e065f7035fb56043a84b1c61334f385d64decd67
edgd1er <edgd1er@hotmail.com>
2021-11-29 01:09:02 +0100

    healt check, openvpn management, loglevel, debug, parameters

--- transmission/start.sh
+++ transmission/start.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-source /etc/openvpn/utils.sh
+set -e
+DEBUG=${DEBUG:-"false"}
+[[ ${DEBUG} != "false" ]] && set -x
+
+#Change timzeone if set
+[[ -n ${TZ} ]] && [[ -e /usr/share/zoneinfo/${TZ} ]] && [[ -w /etc/localtime ]] && rm -f /etc/localtime && ln -s /usr/share/zoneinfo/${TZ} /etc/localtime
 
 # Source our persisted env variables from container startup
 . /etc/transmission/environment-variables.sh
 
-# This script will be called with OpenVPN environment variables
-# See https://openvpn.net/community-resources/reference-manual-for-openvpn-2-4/#scripting-and-environmental-variables
-echo "Up script executed with device=$dev ifconfig_local=$ifconfig_local"
-if [[ "$ifconfig_local" = "" ]]; then
+# Re-create `--up` command arguments to maintain compatibility with old user scripts
+USER_SCRIPT_ARGS=("$dev" "$tun_mtu" "$link_mtu" "$ifconfig_local" "$ifconfig_remote" "$script_context")
+[[ -n $1 ]] && [[ -z ${dev} ]] && dev=$1
+[[ -n $2 ]] && [[ -z ${tun_mtu} ]] && tun_mtu=$2
+[[ -n $3 ]] && [[ -z ${link_mtu} ]] && link_mtu=$3
+[[ -n $4 ]] && [[ -z ${ifconfig_local} ]] && ifconfig_local=$4
+[[ -n $5 ]] && [[ -z ${ifconfig_remote} ]] && ifconfig_remote=$5
+[[ -n $6 ]] && [[ -z ${script_context} ]] && script_context=$6
+
+# This script will be called with tun/tap device name as parameter 1, and local IP as parameter 4
+# See https://openvpn.net/index.php/open-source/documentation/manuals/65-openvpn-20x-manpage.html (--up cmd)
+#echo "Up script executed with $*"
+echo "transmission start script executed with ${USER_SCRIPT_ARGS[*]}"
+echo "0: ${USER_SCRIPT_ARGS[0]}"
+echo "1: ${USER_SCRIPT_ARGS[1]}"
+echo "2: ${USER_SCRIPT_ARGS[2]}"
+echo "3: ${USER_SCRIPT_ARGS[3]}"
+echo "4: ${USER_SCRIPT_ARGS[4]}"
+if [[ "${ifconfig_local}" == "" ]]; then
   echo "ERROR, unable to obtain tunnel address"
   echo "killing $PPID"
   kill -9 $PPID
@@ -19,17 +39,19 @@ fi
 USER_SCRIPT_ARGS=("$dev" "$tun_mtu" "$link_mtu" "$ifconfig_local" "$ifconfig_remote" "$script_context")
 
 # If transmission-pre-start.sh exists, run it
-if [[ -x /scripts/transmission-pre-start.sh ]]; then
-  echo "Executing /scripts/transmission-pre-start.sh"
-  /scripts/transmission-pre-start.sh "${USER_SCRIPT_ARGS[@]}"
-  echo "/scripts/transmission-pre-start.sh returned $?"
+SCRIPT=/etc/scripts/transmission-pre-start.sh
+if [[ -x ${SCRIPT} ]]; then
+  echo "Executing ${SCRIPT}"
+  #${SCRIPT} "$@"
+  ${SCRIPT} "${USER_SCRIPT_ARGS[*]}"
+  echo "${SCRIPT} returned $?"
 fi
 
-echo "Updating TRANSMISSION_BIND_ADDRESS_IPV4 to the ip of $dev : $ifconfig_local"
-export TRANSMISSION_BIND_ADDRESS_IPV4=$ifconfig_local
+echo "Updating TRANSMISSION_BIND_ADDRESS_IPV4 to the ip of ${dev} : ${ifconfig_local}"
+export TRANSMISSION_BIND_ADDRESS_IPV4=${ifconfig_local}
 # Also update the persisted settings in case it is already set. First remove any old value, then add new.
 sed -i '/TRANSMISSION_BIND_ADDRESS_IPV4/d' /etc/transmission/environment-variables.sh
-echo "export TRANSMISSION_BIND_ADDRESS_IPV4=$ifconfig_local" >>/etc/transmission/environment-variables.sh
+echo "export TRANSMISSION_BIND_ADDRESS_IPV4=${ifconfig_local}" >>/etc/transmission/environment-variables.sh
 
 if [[ "combustion" = "$TRANSMISSION_WEB_UI" ]]; then
   echo "Using Combustion UI, overriding TRANSMISSION_WEB_HOME"
@@ -95,10 +117,11 @@ if [[ -x /etc/openvpn/${OPENVPN_PROVIDER,,}/update-port.sh && (-z $DISABLE_PORT_
 fi
 
 # If transmission-post-start.sh exists, run it
-if [[ -x /scripts/transmission-post-start.sh ]]; then
-  echo "Executing /scripts/transmission-post-start.sh"
-  /scripts/transmission-post-start.sh "${USER_SCRIPT_ARGS[@]}"
-  echo "/scripts/transmission-post-start.sh returned $?"
+SCRIPT=/etc/scripts/transmission-post-start.sh
+if [[ -x ${SCRIPT} ]]; then
+  echo "Executing ${SCRIPT}"
+  ${SCRIPT} "${USER_SCRIPT_ARGS[*]}"
+  echo "${SCRIPT} returned $?"
 fi
 
 echo "Transmission startup script complete."
